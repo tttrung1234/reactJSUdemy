1f9d9ba774e1fa6d6a7e1cb85f1c8b3122d0ce7d
Trung <tttrung01121999@gmail.com>
2022-03-03 21:51:50 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    
+    
+   render() {
+      
+      return (
+          
+         <div>
+
+             <Header/>
+             <Content/>
+         </div>
+       
+         
+      );
+   }
+
+}
+
+
+
+
+class Header extends React.Component{
+    render() {
+        return(
+           
+
+        <div>
+            <h1>header</h1>
+            
+        </div>
+
+        );
+    }
+}
+
+class Content extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>content</h1>
+            </div>
+        );
+    }
+}
+
+
+export default App;
